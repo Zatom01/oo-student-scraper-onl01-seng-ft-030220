90adaa9bea1af6a61a376627462b0cbc47d5a52c
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-22 21:43:00 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,36 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
+
+url="https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
 
 class Scraper
 
+  @@arr=[]
+
   def self.scrape_index_page(index_url)
     
+
+    html=open(index_url)
+    doc=Nokogiri::HTML(html)
+    @element_arr=doc.css('div.student-card')
+    @profile={}
+
+    # @url="students/"+element_arr.css('h4.student-name').text.split(" ").join("-")+".html"
+
+    @element_arr.each do |each|
+
+      @profile={:name=>each.css('h4.student-name').text,
+      :location=>each.css('p.student-location').text,
+      :profile_url=>"students/"+each.css('h4.student-name').text.split(" ").join("-")+".html"}
+
+      @@arr << @profile
+
+    end
+
+    
+    return @@arr
   end
 
-  def self.scrape_profile_page(profile_url)
-    
-  end
 
 end
-
