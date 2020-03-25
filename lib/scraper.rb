e44da6e50812ac29cbe1641763153363eb28821b
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-25 20:46:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ require 'pry'
 class Scraper
   
   def self.scrape_index_page(url)
-    index_page = Nokogiri::HTML(open(index_url))
+    index_page = Nokogiri::HTML(open(url))
     students = []
     index_page.css("div.roster-cards-container").each do |card|
       card.css(".student-card a").each do |student|
