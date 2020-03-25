c8d9093e13dc255918c1e03f73eeaee0bc930d33
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-25 20:36:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,20 +2,20 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-url="https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
+# url="https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
 
 class Scraper
   index_page = Nokogiri::HTML(open(index_url))
   students = []
   index_page.css("div.roster-cards-container").each do |card|
     card.css(".student-card a").each do |student|
-    student_profile_link = "#{student.attr('href')}"
-    student_location = student.css('.student-location').text
-    student_name = student.css('.student-name').text
-    students << {name: student_name, location: student_location, profile_url: student_profile_link}
-        end
+      student_profile_link = "#{student.attr('href')}"
+      student_location = student.css('.student-location').text
+      student_name = student.css('.student-name').text
+      students << {name: student_name, location: student_location, profile_url: student_profile_link}
       end
-    students
+    end
+  students
 
   
 end 
