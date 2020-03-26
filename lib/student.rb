8f975b9707c8e60683f5eef5901a5bfdb26cadb3
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-26 00:25:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,11 +10,15 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |hash|
+      self.new(hash)
+    end
     
   end
 
   def add_student_attributes(attributes_hash)
+    attributes_hash.each {|k,v| self.send (("#{k}="),v)}
+    
     
   end
 
