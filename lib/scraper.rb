bd740d2de05273f29507c2b15be5d3a8ed70b0ce
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-26 00:29:41 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,35 +2,63 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-url="https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
 
 class Scraper
-
-  @@scraped_students=[]
-
-  def self.scrape_index_page(index_url)
-    
-
-    html=open(index_url)
-    doc=Nokogiri::HTML(html)
-    @element_arr=doc.css('div.student-card')
-    @profile={}
-
-    # @url="students/"+element_arr.css('h4.student-name').text.split(" ").join("-")+".html"
-
-    @element_arr.each do |each|
-
-      @profile={:name=>each.css('h4.student-name').text,
-      :location=>each.css('p.student-location').text,
-      :profile_url=>"students/"+each.css('h4.student-name').text.split(" ").join("-")+".html"}
-
-      @@scraped_students << @profile
-
+  
+  def self.scrape_index_page(url)
+    index_page = Nokogiri::HTML(open(url))
+    students = []
+    index_page.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_profile_link = "#{student.attr('href')}"
+        student_location = student.css('.student-location').text
+        student_name = student.css('.student-name').text
+        students << {name: student_name, location: student_location,profile_url: student_profile_link}
+      end
+    end
+    students
+  end 
+  
+  def self.scrape_profile_page(profile_url)
+    profile={}
+    doc=Nokogiri::HTML(open(profile_url))
+    doc.css("div.social-icon-container a").each do |link|
+      if link.attribute("href").value.include?("twitter")
+        profile[:twitter] = link.attribute("href").value
+      elsif link.attribute("href").value.include?("linkedin")
+        profile[:linkedin] = link.attribute("href").value
+      elsif link.attribute("href").value.include?("github")
+        profile[:github] = link.attribute("href").value
+      else
+        profile[:blog] = link.attribute("href").value
+      end
     end
 
+    profile[:profile_quote] = doc.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
+    profile[:bio] = doc.css("div.main-wrapper.profile .description-holder p").text
+
+    profile
     
-    return @@scraped_students
-  end
+  end 
+end 
 
+    
+# @@scraped_students=[]
 
-end
+#   def self.scrape_index_page(index_url)
+  
+#     html=open(index_url)
+#     doc=Nokogiri::HTML(html)
+#     element_arr=doc.css('div.student-card')
+
+#     # @url="students/"+element_arr.css('h4.student-name').text.split(" ").join("-")+".html"
+
+#     element_arr.each do |each|
+#       student_name=each.css('.student-name').text
+#       student_location=each.css('p.student-location').text
+#       student_url_profile="students/"+each.css('h4.student-name').text.split(" ").join("-")+".html"
+      
+#       @@scraped_students << {:name=> student_name, :location=> student_location, :profile_url=> student_url_profile}
+#     end
+#     @@scraped_students
+#   end
