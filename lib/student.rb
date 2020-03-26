dd7a1a74d565ac761c49246ac3578929e5544ba0
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-26 00:29:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,19 +1,18 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
     student_hash.each {|k, v| self.send(("#{k}="), v)}
-    self << @@all 
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |hash|
-      self.new(hash)
+    students_array.each do |student_hash|
+      self.new(student_hash)
     end
-    
   end
 
   def add_student_attributes(attributes_hash)
@@ -25,7 +24,5 @@ class Student
 
   def self.all
     @@all
-    
   end
 end
-
